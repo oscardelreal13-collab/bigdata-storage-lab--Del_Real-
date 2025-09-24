6e2d7b57e868d1df4c1d994992e9242e82e48f1e
oscardelreal13-collab <oscardelreal13@gmail.com>
2025-09-24 14:49:01 +0200

    Create transform.py

---
+++ src/transform.py
@@ -0,0 +1,7 @@
+"""
+Módulo de transformación y normalización de datos (bronze → silver).
+"""
+
+def transform_data(df):
+    # TODO: Implementar normalización de columnas, tipos y codificación
+    return df
