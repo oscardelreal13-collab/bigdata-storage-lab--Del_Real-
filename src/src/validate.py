cc8043663157b927f67043437601740187bd1615
oscardelreal13-collab <oscardelreal13@gmail.com>
2025-09-25 15:05:11 +0200

    Delete src/src directory

--- src/src/validate.py
+++
@@ -1,8 +0,0 @@
-# src/validate.py
-
-def basic_checks():
-    """
-    Ejemplo de función de validación.
-    Aquí puedes poner tus comprobaciones reales.
-    """
-    print("✔ Validaciones básicas ejecutadas correctamente.")
